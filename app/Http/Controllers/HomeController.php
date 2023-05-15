c45a093e10b0e9538f9187eacf655e1494cde652
jipsnav <85476786+jipsnav@users.noreply.github.com>
2023-05-15 21:12:11 +0800

    POST CRUD implemented

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Posts;
 
 class HomeController extends Controller
 {
   public function home() {
-    return view('pages.home');
+    // fetch all posts
+    $posts = Posts::all();
+    // return view with posts
+    return view('pages.home', compact('posts'));
   }
 
   public function about() {
